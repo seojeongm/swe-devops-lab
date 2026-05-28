3542740debb763a0edb5c5f04dec1bb4205659cd
c5ln <cjstjdgh0209@gmail.com>
2026-05-28 16:55:26 +0900

    github login

--- Practice/src/calc/Matrix.java
+++ Practice/src/calc/Matrix.java
@@ -12,6 +12,12 @@ public class Matrix {
     public void setMatrix(int[][] matrix) {
         this.matrix = matrix;
     }
-
+    public void printMatrix() {
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+        }
+    }
 }
 
